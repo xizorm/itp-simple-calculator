c7decd4187e8b09ae188cecb8c88ca8b145b6997
xizorm <68193101+xizorm@users.noreply.github.com>
2025-12-30 21:00:08 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,27 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x-y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        print("You can't divide by 0!"
+    else: 
+        return x/y
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x * x
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return sqr(x)
